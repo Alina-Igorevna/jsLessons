f903222dc60aaf15130ac2bf7977eeb12e453bc9
Alina-Igorevna <Chuveva92@yandex.ru>
2020-04-30 12:24:50 +0200

    init projekt

---
+++ script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+let ansvers = [],
+    questions = [
+      'Как ваше имя?',
+      'Как ваша фамилия?',
+      'Сколько вам лет?'
+    ];
+
+let person = {
+  name: "John",
+  age: 25,
+
+}
+
+cin
